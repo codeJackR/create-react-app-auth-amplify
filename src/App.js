c36d5a132e0ac62df000ac956a0f4121d2fff9f4
CodeJackR <rp7@Rushabhs-MacBook-Pro.local>
2022-12-22 21:29:24 +0530

    Search feature

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import {NavBar, BrowseInfluencers} from "./ui-components";
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function App() {
+
+  const navbarOverrides = {
+    "image": {
+        src: "https://cdn.vectorstock.com/i/1000x1000/39/94/varicella-zoster-virus-glyph-icon-vector-33723994.webp"
+    }
   }
+
+  return (
+    <div className="App">
+      <NavBar overrides={navbarOverrides} width="100%" />
+      <header className="App-header">
+        <h1>InfluNet</h1>
+        <BrowseInfluencers />
+      </header>
+    </div>
+  );
 }
 
-export default withAuthenticator(App);
+export default App;
